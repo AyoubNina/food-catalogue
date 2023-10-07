15e05fff7cb223d0639a305dfd47f8cfa26837f2
AyoubNina <jihene.ayoub@gmail.com>
2023-10-07 11:02:27 +0100

    last commit

---
+++ src/main/java/com/sesame/foodcatalogue/mapper/FoodItemMapper.java
@@ -0,0 +1,19 @@
+package com.sesame.foodcatalogue.mapper;
+
+
+import com.sesame.foodcatalogue.dto.FoodItemDTO;
+import com.sesame.foodcatalogue.entity.FoodItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface FoodItemMapper {
+
+    FoodItemMapper INSTANCE = Mappers.getMapper(FoodItemMapper.class);
+
+    FoodItem mapFoodItemDTOToFoodItem(FoodItemDTO foodItemDTO);
+
+    FoodItemDTO mapFoodItemToFoodItemDto(FoodItem foodItem);
+
+
+}
